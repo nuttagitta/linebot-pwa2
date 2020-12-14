2c1d5c92a07bc1d439cdfc02658057119eac7c06
nuttagitta <74849705+nuttagitta@users.noreply.github.com>
2020-12-14 17:48:57 +0700

    edit
    editcode

--- bot.php
+++ bot.php
@@ -16,7 +16,7 @@ $request_array = json_decode($request, true);   // Decode JSON to Array
 if($message == "ค้นหาใบแจ้งหนี้"){
     $arrayPostData['to'] = $id;
     $arrayPostData['messages'][0]['type'] = "text";
-    $arrayPostData['messages'][0]['text'] = "ใบแจ้งหนี้ คลิกที่นี่ https://pwa.thailawyersoft.com/Output/202012/PDF/11332222530.pdf ผู้ใช้น้ำสามารถสแกนหรือคลิกลิงก์เพื่อรับใบแจ้งหนี้ได้ทันที";
+    $arrayPostData['messages'][0]['text'] = "ใบแจ้งหนี้ คลิกที่นี่ ผู้ใช้น้ำสามารถสแกนหรือคลิกลิงก์เพื่อรับใบแจ้งหนี้ได้ทันที";
     $arrayPostData['messages'][1]['stickerId'] = "34";
     pushMsg($arrayHeader,$arrayPostData);
 }
