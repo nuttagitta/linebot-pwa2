dcdb7deebb419e38809db805ee4936c0cacbe01b
nuttagitta <74849705+nuttagitta@users.noreply.github.com>
2020-12-19 15:45:40 +0700

    edit

--- bot.php
+++ bot.php
@@ -12,14 +12,21 @@ $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
-
-if($message == "ทดสอบ"){
-    $arrayPostData['to'] = $id;
-    $arrayPostData['messages'][0]['type'] = "text";
-    $arrayPostData['messages'][0]['text'] = "ทดสอบข้อตวาม";
-    pushMsg($arrayHeader,$arrayPostData);
+if ( sizeof($request_array['events']) > 0 ) {
+   foreach ($request_array['events'] as $event) {
+      
+      $reply_message = '';
+      $reply_token = $event['replyToken'];
+      $text = $event['message']['text'];
+      $data = [
+         'replyToken' => $reply_token,
+         'messages' => [['type' => 'text', 'text' => $text ]]
+      ];
+      $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+      $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);
+      echo "Result: ".$send_result."\r\n";
+    }
 }
-echo "OK";
 
 
 
